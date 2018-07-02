d32117c58333f4f7674c90c90da6e007083ea648
Dearzhumengren <861466264@qq.com>
2018-07-02 22:46:01 +0800

    reglogin change

---
+++ app/index/controller/User.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/2
+ * Time: 22:37
+ */
+namespace app\index\controller;
+use think\Controller;
+
+class User extends Controller
+{
+    public function index()
+    {
+
+    }
+    public function reg()
+    {
+
+    }
+}
